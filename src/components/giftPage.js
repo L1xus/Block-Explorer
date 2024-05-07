0071ebe6bc7c988dd2fa1951b53397ec05cfaa2d
L1xus <omarstellar456@gmail.com>
2024-05-07 17:27:54 +0100

    gibaway claimed!

--- src/components/giftPage.js
+++ src/components/giftPage.js
@@ -2,12 +2,8 @@ export default function GiftPage() {
     return (
         <div className='Header'>
           <div className='giftWinner'>
-              <h2>Congratulations you are the first one to make it!</h2>
-              <h2>You have won $10</h2>
-              <h3>
-                  Send me your Solana Address to this email with your Twitter handle:
-                  <a href="mailto:omarstellar456@gmail.com"> omarstellar456@gmail.com</a>
-              </h3>
+              <h2>The reward has been claimed by @ahr2510!</h2>
+              <img src={process.env.PUBLIC_URL + '/gibaway.jpg'} alt='gibaway-winner' id='gibWinner'></img>
           </div>
           <div className="koksal">
             <img src="https://media1.tenor.com/m/YbXVv1nPb7oAAAAd/koksal-koksalbaba.gif" alt="Koksal Koksalbaba GIF" className="koksal"></img>
